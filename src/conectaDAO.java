c4c135dea1d7530c0c42fe6f7af7d28475824c9d
Matheus Leal <lleal.dev@gmail.com>
2024-12-27 14:15:00 -0300

    Implementado metodo de desconctar com o Banco de Dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -7,11 +7,11 @@ import javax.swing.JOptionPane;
 
 
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
+    Connection conn;
         
-        try {
+    public  Connection connectDB(){
+         conn = null;
+         try {
         
             conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=1");
             
@@ -21,4 +21,14 @@ public class conectaDAO {
         return conn;
     }
     
+     public void desconectar(){
+        try {
+              if (conn != null && !conn.isClosed()) {
+                  conn.close();
+                  System.out.println("Voce se desconectou do banco de dados.");
+              }
+          } catch (SQLException ex) {
+              System.out.println("Nao foi possivel desconectar do banco dados.");
+          }
+    }
 }
