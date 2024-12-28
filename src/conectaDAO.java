eb963628cead16d5933ef0714d8b4e0f54cb4680
Matheus Leal <lleal.dev@gmail.com>
2024-12-27 22:02:32 -0300

    Desabilitando o uso de SSL do MySQL

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,31 +4,30 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
 public class conectaDAO {
+
     Connection conn;
-        
-    public  Connection connectDB(){
-         conn = null;
-         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=1");
-            
-        } catch (SQLException erro){
+
+    public Connection connectDB() {
+        conn = null;
+        try {
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=1&useSSL=false");
+
+        } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
     }
-    
-     public void desconectar(){
+
+    public void desconectar() {
         try {
-              if (conn != null && !conn.isClosed()) {
-                  conn.close();
-                  System.out.println("Voce se desconectou do banco de dados.");
-              }
-          } catch (SQLException ex) {
-              System.out.println("Nao foi possivel desconectar do banco dados.");
-          }
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Voce se desconectou do banco de dados.");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Nao foi possivel desconectar do banco dados.");
+        }
     }
 }
